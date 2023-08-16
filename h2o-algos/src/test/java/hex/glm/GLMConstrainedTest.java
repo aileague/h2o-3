2d5f263538d488b5a7850a207ef0b6e625241eb3
wendycwong <wendycwong@yahoo.com>
2023-08-16 12:57:03 -0700

    add redundant constraints tests.

--- h2o-algos/src/test/java/hex/glm/GLMConstrainedTest.java
+++ h2o-algos/src/test/java/hex/glm/GLMConstrainedTest.java
@@ -31,6 +31,7 @@ public class GLMConstrainedTest extends TestUtil {
   Frame _linearConstraint1;
   Frame _linearConstraint2;
   Frame _linearConstraint3;
+  Frame _linearConstraint4;
   List<String> _coeffNames1;
   String[][] _betaConstraintNames1;
   double[][] _betaConstraintValStandard1;
@@ -56,8 +57,41 @@ public class GLMConstrainedTest extends TestUtil {
     generateConstraint1FrameNAnswer(train);
     generateConstraint2FrameNAnswer(train);
     generateConstraint3FrameNAnswer();
+    generateConstraint4FrameNAnswer();
   }
-  
+
+
+  public void generateConstraint4FrameNAnswer() {
+    // Constraints in the linear_constraints, 
+    // a. -0.3*beta_0+0.5*beta_1+1*beta_3-3 <= 0; 
+    // b. 3*beta_8-4*beta_36+0.5*beta_37 <= 0, 
+    // c.0.1*beta_38-0.2*beta_39==0, 
+    // d. 2*beta_40-0.1*beta_41-0.4*beta_42+0.8 <= 0;
+    // e. 0.1*beta_4-0.5*beta_5+0.7*beta_6-1.1 == 0; 
+    // f. 2*beta_6+0.5*beta_43-0.3*beta_7 == 0 
+    // g. 0.5*beta_36-1.5*beta_38-0.3 == 0    
+    // h. -beta_1+betaCoefLen-3-9.0 <= 0
+    int coefLen = _coeffNames1.size();
+    _linearConstraint4 = new TestFrameBuilder()
+            .withColNames("names", "values", "types", "constraint_numbers")
+            .withVecTypes(T_STR, T_NUM, T_STR, T_NUM)
+            .withDataForCol(0, new String[] {_coeffNames1.get(0), _coeffNames1.get(1), _coeffNames1.get(3),
+                    "constant", _coeffNames1.get(8), _coeffNames1.get(36), _coeffNames1.get(37), _coeffNames1.get(38),
+                    _coeffNames1.get(39), _coeffNames1.get(40), _coeffNames1.get(41), _coeffNames1.get(42), "constant",
+                    _coeffNames1.get(4), _coeffNames1.get(5), _coeffNames1.get(6), "constant", _coeffNames1.get(6),
+                    _coeffNames1.get(43), _coeffNames1.get(7), _coeffNames1.get(36), _coeffNames1.get(38), "constant",
+                    _coeffNames1.get(1), _coeffNames1.get(coefLen-3), "constant"})
+            .withDataForCol(1, new double [] {-0.3, 0.5, 1.0, -3.0, 3, -4, 0.5, 0.1, -0.2, 2.0, -0.1, -0.4,
+                    0.8, 0.1, -0.5, 0.7, -1.1, 2.0, 0.5, -0.3, 0.5, -1.5, -0.3, -1.0, 1.0, -9.0})
+            .withDataForCol(2, new String[] {"lessthanequal", "lessthanequal", "lessthanequal", "lessthanequal",
+                    "lessthanequal", "lessthanequal", "lessthanequal", "equal", "equal", "lessthanequal",
+                    "lessthanequal", "lessthanequal", "lessthanequal", "equal", "equal", "equal", "equal", "equal",
+                    "equal", "equal", "equal", "equal", "equal", "lessthanequal", "lessthanequal", "lessthanequal"})
+            .withDataForCol(3, new int[]{0, 0, 0, 0, 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6,
+                    6, 7, 7 ,7}).build();
+    Scope.track(_linearConstraint2);
+  }
+
   public void generateConstraint2FrameNAnswer(Frame train) {
     // Constraints in the linear_constraints, 
     // a. -0.3*beta_0+0.5*beta_1+1*beta_3-3 <= 0; 
@@ -217,7 +251,7 @@ public class GLMConstrainedTest extends TestUtil {
 
   // linear constraints with two duplicated constraints
   @Test
-  public void testDuplicateConstraints() {
+  public void testDuplicateLinearConstraints() {
     Scope.enter();
     try {
       Frame train = parseAndTrackTestFile("smalldata/glm_test/gaussian_20cols_10000Rows.csv");
@@ -234,6 +268,33 @@ public class GLMConstrainedTest extends TestUtil {
       params._max_iterations = 1;
       params._expose_constraints = true;
       params._linear_constraints = linear_constraints._key;
+      GLMModel glm2 = new GLM(params).trainModel().get();
+      assert 1==2 : "Should have thrown an error due to duplicated constraints.";
+    } catch(Exception ex) {
+
+    } finally {
+      Scope.exit();
+    }
+  }
+
+  @Test
+  public void testDuplicateBetaLinearConstraints() {
+    Scope.enter();
+    try {
+      Frame train = parseAndTrackTestFile("smalldata/glm_test/gaussian_20cols_10000Rows.csv");
+      int[] catCol = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+      for (int colInd : catCol)
+        train.replace((colInd), train.vec(colInd).toCategoricalVec()).remove();
+      DKV.put(train);
+      GLMModel.GLMParameters params = new GLMModel.GLMParameters(gaussian);
+      params._standardize = false;
+      params._response_column = "C21";
+      params._solver = IRLSM;
+      params._train = train._key;
+      params._max_iterations = 1;
+      params._expose_constraints = true;
+      params._linear_constraints = _linearConstraint4._key;
+      params._beta_constraints = _betaConstraint1._key;
       GLMModel glm2 = new GLM(params).trainModel().get();
       assert 1==2 : "Should have thrown an error due to duplicated constraints.";
     } catch(Exception ex) {
